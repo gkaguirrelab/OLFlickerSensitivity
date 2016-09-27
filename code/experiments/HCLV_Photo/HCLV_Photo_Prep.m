8a9a63fd3b7ce6552271ef0b869ab5eaed645353
Manuel Spitschan <mspits@sas.upenn.edu>
2016-09-27 16:14:16 -0400

    Implemented spectrum-seeking for connectome
    Signed-off-by: Manuel Spitschan <mspits@sas.upenn.edu>

--- code/experiments/HCLV_Photo/HCLV_Photo_Prep.m
+++ code/experiments/HCLV_Photo/HCLV_Photo_Prep.m
@@ -128,7 +128,65 @@ switch optIndex
         [cacheData, olCache, params] = OLReceptorIsolateFindIsolatingPrimarySettings(params, true);
         OLReceptorIsolateSaveCache(cacheData, olCache, params);
         
+    case 3
         
+        commandwindow;
+        observerID = GetWithDefault('>> Enter <strong>user name</strong>', 'HERO_test');
+        observerAgeInYrs = GetWithDefault('>> Enter <strong>observer age</strong>:', 32);
+        todayDate = datestr(now, 'mmddyy');
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % Correct the spectrum
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        tic;
+        theCalType = params.calibrationType;
+        spectroRadiometerOBJ = [];
+        spectroRadiometerOBJWillShutdownAfterMeasurement = false;
+        theDirections = {'LightFluxXEccentricity' 'LMinusMDirectedXEccentricity' 'SDirectedXEccentricity'};
+        theDirectionsCorrect = [true true false false];
+        cacheDir = getpref('OneLight', 'cachePath');
+        materialsPath = getpref('OneLight', 'materialsPath');
+        
+        for d = 1:length(theDirections)
+            % Print out some information
+            fprintf(' * Direction:\t<strong>%s</strong>\n', theDirections{d});
+            fprintf(' * Observer:\t<strong>%s</strong>\n', observerID);
+            fprintf(' * Date:\t<strong>%s</strong>\n', todayDate);
+            
+            % Correct the cache
+            fprintf(' * Starting spectrum-seeking loop...\n');
+            [cacheData olCache spectroRadiometerOBJ] = OLCorrectCacheFileOOC(...
+                fullfile(cacheDir, 'stimuli', ['Cache-' theDirections{d} '.mat']), ...
+                'igdalova@mail.med.upenn.edu', ...
+                'PR-670', spectroRadiometerOBJ, spectroRadiometerOBJWillShutdownAfterMeasurement, ...
+                'FullOnMeas', false, ...
+                'CalStateMeas', false, ...
+                'DarkMeas', false, ...
+                'REFERENCE_OBSERVER_AGE', observerAgeInYrs, ...
+                'ReducedPowerLevels', false, ...
+                'selectedCalType', theCalType, ...
+                'CALCULATE_SPLATTER', false, ...
+                'lambda', 0.8, ...
+                'NIter', 10, ...
+                'powerLevels', [0 1.0000], ...
+                'doCorrection', theDirectionsCorrect(d), ...
+                'outDir', fullfile(materialsPath, 'PIPRMaxPulse', todayDate));
+            fprintf(' * Spectrum seeking finished!\n');
+            
+            % Save the cache
+            fprintf(' * Saving cache ...');
+            params = cacheData.data(observerAgeInYrs).describe.params;
+            params.modulationDirection = theDirections{d};
+            params.cacheFile = ['Cache-' params.modulationDirection '_' observerID '_' todayDate '.mat'];
+            OLReceptorIsolateSaveCache(cacheData, olCache, params);
+            fprintf('done!\n');
+        end
+        
+        if (~isempty(spectroRadiometerOBJ))
+            spectroRadiometerOBJ.shutDown();
+            spectroRadiometerOBJ = [];
+        end
+        toc;
         
     case 4
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
