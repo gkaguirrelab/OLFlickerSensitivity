6075e0c6cad49c2efb54b0b8682846cf7c54bd8b
melanopsin <melanopsin@blk137.wireless-pennnet.upenn.edu>
2016-10-05 14:20:25 -0400

    Updating rod control prep

--- code/experiments/MelanopsinMRRodControl/MelanopsinMRRodControl_PrepOOC.m
+++ code/experiments/MelanopsinMRRodControl/MelanopsinMRRodControl_PrepOOC.m
@@ -12,7 +12,7 @@ params.CALCULATE_SPLATTER = false;
 params.maxPowerDiff = 10^(-1);
 params.photoreceptorClasses = 'LConeTabulatedAbsorbance,MConeTabulatedAbsorbance,SConeTabulatedAbsorbance,Melanopsin,Rods';
 params.fieldSizeDegrees = 27.5;
-params.pupilDiameterMm = 8;
+params.pupilDiameterMm = 6;
 params.isActive = 1;
 params.useAmbient = 1;
 params.REFERENCE_OBSERVER_AGE = 32;
@@ -22,9 +22,9 @@ params.primaryHeadRoom = 0.01;
 %% Silent substitution
 %% MaxMel
 params.pegBackground = false;
-params.modulationDirection = {'RodDirected'};
-params.modulationContrast = [];
-params.whichReceptorsToIsolate = {[5]};
+params.modulationDirection = {'MelanopsinDirected'};
+params.modulationContrast = [2/3];
+params.whichReceptorsToIsolate = {[4]};
 params.whichReceptorsToIgnore = {[]};
 params.whichReceptorsToMinimize = {[]};
 params.directionsYoked = [0];
@@ -32,32 +32,29 @@ params.directionsYokedAbs = [0];
 params.receptorIsolateMode = 'Standard';
 
 % Mel shifted background
-params.backgroundType = 'BackgroundOptim';
+params.backgroundType = 'BackgroundMaxMel';
 params.cacheFile = ['Cache-' params.backgroundType  '.mat'];
 [cacheDataBackground, olCache, params] = OLReceptorIsolateMakeBackground(params, true);
 OLReceptorIsolateSaveCache(cacheDataBackground, olCache, params);
 
 % Now, make the modulation
 params.primaryHeadRoom = 0.005;
-params.backgroundType = 'BackgroundOptim';
-params.modulationDirection = 'RodDirected';
-params.modulationContrast = [];
-params.whichReceptorsToIsolate = [5];
-params.whichReceptorsToIgnore = [];
+params.backgroundType = 'BackgroundMaxMel';
+params.modulationDirection = 'MelanopsinDirectedSuperMaxMel';
+params.modulationContrast = [2/3];
+params.whichReceptorsToIsolate = [4];
+params.whichReceptorsToIgnore = [5];
 params.whichReceptorsToMinimize = [];
 params.receptorIsolateMode = 'Standard';
 params.cacheFile = ['Cache-' params.modulationDirection '.mat'];
 [cacheDataMaxMel, olCacheMaxMel, paramsMaxMel] = OLReceptorIsolateFindIsolatingPrimarySettings(params, true);
-
-%paramsMaxMel.modulationDirection = 'MelanopsinDirectedSuperMaxMel';
-%paramsMaxMel.cacheFile = ['Cache-' paramsMaxMel.modulationDirection '.mat'];
 %OLReceptorIsolateSaveCache(cacheDataMaxMel, olCacheMaxMel, paramsMaxMel);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Silent substitution
 % Now, make the modulation
 params.primaryHeadRoom = 0.005;
-params.backgroundType = 'BackgroundOptim';
+params.backgroundType = 'BackgroundMaxMel';
 params.modulationDirection = 'LMinusMDirected';
 params.modulationContrast = [0.06 -0.06];
 params.whichReceptorsToIsolate = [1 2];
