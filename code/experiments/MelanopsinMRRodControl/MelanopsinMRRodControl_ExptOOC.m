b65298e31addccbf7ec7416a8eb547795ad09de7
Manuel Spitschan <mspits@sas.upenn.edu>
2016-10-06 15:35:00 -0400

    BF: Updates to ExptOOC for rod control
    Signed-off-by: Manuel Spitschan <mspits@sas.upenn.edu>

--- code/experiments/MelanopsinMRRodControl/MelanopsinMRRodControl_ExptOOC.m
+++ code/experiments/MelanopsinMRRodControl/MelanopsinMRRodControl_ExptOOC.m
@@ -7,100 +7,132 @@ observerID = GetWithDefault('>> Enter <strong>user name</strong>', 'HERO_test');
 observerAgeInYrs = GetWithDefault('>> Enter <strong>observer age</strong>:', 32);
 todayDate = datestr(now, 'mmddyy');
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Correct the spectrum - not filtered
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-tic;
-theCalType = 'BoxBRandomizedLongCableDStubby1_ND00';
+%% Set up the cal
+theCalType = 'BoxBRandomizedLongCableDStubby1_ND02';
+cacheDir = fullfile(getpref('OneLight', 'cachePath'), 'stimuli');
+cal = LoadCalFile(theCalType, [], getpref('OneLight', 'OneLightCalData'));
+
+%% Load the cache files
+% MaxMel
+olCache1 = OLCache(cacheDir, cal);
+params1.modulationDirection = 'MelanopsinDirectedRodControl';
+params1.cacheFile = ['Cache-' params2.modulationDirection '.mat'];
+cacheData1 = olCache.load(params1.cacheFile);
+params1.cacheFile = ['Cache-' params2.modulationDirection '_' observerID '_' todayDate '.mat'];
+
+% L-M
+olCache2 = OLCache(cacheDir, cal);
+params2.modulationDirection = 'LMinusMDirectedRodControl';
+params2.cacheFile = ['Cache-' params2.modulationDirection '.mat'];
+cacheData2 = olCache.load(params2.cacheFile);
+params2.cacheFile = ['Cache-' params2.modulationDirection '_' observerID '_' todayDate '.mat'];
+
+% Get the photoreceptors
+theCanonicalPhotoreceptors = cacheData1.data(observerAgeInYrs).describe.photoreceptors;
+T_receptors = cacheData1.data(observerAgeInYrs).describe.T_receptors;
+postreceptoralCombinations = [1 1 1 0 0 ; 1 -1 0 0 0 ; 0 0 1 0 0 ; 0 0 0 10 ; 0 0 0 0 1];
+
+%% Correct the spectra
+primaryValues = [cacheDataMaxMel.data(observerAgeInYrs).backgroundPrimary ...
+    cacheDataMaxMel.data(observerAgeInYrs).modulationPrimarySignedPositive ...
+    cacheDataMaxMel.data(observerAgeInYrs).modulationPrimarySignedNegative ...
+    cacheDataLMinusM.data(observerAgeInYrs).modulationPrimarySignedPositive ...
+    cacheDataLMinusM.data(observerAgeInYrs).modulationPrimarySignedNegative];
+NIter = 5;
+lambda = 0.8;
+NDFilter = [];
+cacheDataMaxMel.cal
+meterType = 'PR-670';
 spectroRadiometerOBJ = [];
-spectroRadiometerOBJWillShutdownAfterMeasurement = false;
-theDirections = {'MelanopsinDirectedRodControl' 'LMinusMDirectedRodControl'};
+spectroRadiometerOBJWillShutdownAfterMeasurement = true;
 
-theDirectionsCorrect = [true true false false];
-cacheDir = getpref('OneLight', 'cachePath');
-materialsPath = getpref('OneLight', 'materialsPath');
+% Run the correction
+[correctedPrimaryValues primariesCorrectedAll deltaPrimariesCorrectedAll measuredSpd measuredSpdRaw predictedSpd] = OLCorrectPrimaryValues(cal, primaryValues, NIter, lambda, NDFilter, ...
+    meterType, spectroRadiometerOBJ, spectroRadiometerOBJWillShutdownAfterMeasurement);
 
-%% NOT FILTERED
-for d = 1:length(theDirections)
-    % Print out some information
-    fprintf(' * Direction:\t<strong>%s</strong>\n', theDirections{d});
-    fprintf(' * Observer:\t<strong>%s</strong>\n', observerID);
-    fprintf(' * Date:\t<strong>%s</strong>\n', todayDate);
-    
-    % Correct the cache
-    fprintf(' * Starting spectrum-seeking loop...\n');
-    [cacheData olCache spectroRadiometerOBJ] = OLCorrectCacheFileOOC(...
-        fullfile(cacheDir, 'stimuli', ['Cache-' theDirections{d} '.mat']), ...
-        'igdalova@mail.med.upenn.edu', ...
-        'PR-670', spectroRadiometerOBJ, spectroRadiometerOBJWillShutdownAfterMeasurement, ...
-        'FullOnMeas', false, ...
-        'CalStateMeas', false, ...
-        'DarkMeas', false, ...
-        'REFERENCE_OBSERVER_AGE', observerAgeInYrs, ...
-        'ReducedPowerLevels', false, ...
-        'selectedCalType', theCalType, ...
-        'CALCULATE_SPLATTER', false, ...
-        'lambda', 0.8, ...
-        'NIter', 10, ...
-        'powerLevels', [0 1.0000], ...
-        'doCorrection', theDirectionsCorrect(d), ...
-        'postreceptoralCombinations', [1 1 1 0 0 ; 1 -1 0 0 0 ; 0 0 1 0 0 ; 0 0 0 1 0 ; 0 0 0 0 1], ...
-        'outDir', fullfile(materialsPath, 'PIPRMaxPulse', todayDate));
-    fprintf(' * Spectrum seeking finished!\n');
-    
-    % Save the cache
-    fprintf(' * Saving cache ...');
-    params = cacheData.data(observerAgeInYrs).describe.params;
-    params.modulationDirection = theDirections{d};
-    params.cacheFile = ['Cache-' params.modulationDirection '_' observerID '_' todayDate '.mat'];
-    OLReceptorIsolateSaveCache(cacheData, olCache, params);
-    fprintf('done!\n');
+% Calculate the contrasts
+for iter = 1:NIter
+    % Save out information about the correction
+    [contrastsPositive1(:, iter) postreceptoralContrastsPositive1(:, iter)] = ComputeAndReportContrastsFromSpds(['Iteration ' num2str(iter, '%02.0f')] ,theCanonicalPhotoreceptors,T_receptors,...
+        measuredSpd{1}(:, end), measuredSpd{2}(:, end), postreceptoralCombinations, true);
+    [contrastsNegative1(:, iter) postreceptoralContrastsNegative1(:, iter)] = ComputeAndReportContrastsFromSpds(['Iteration ' num2str(iter, '%02.0f')] ,theCanonicalPhotoreceptors,T_receptors,...
+        measuredSpd{1}(:, end), measuredSpd{3}(:, end), postreceptoralCombinations, true);
+    [contrastsPositive2(:, iter) postreceptoralContrastsPositive2(:, iter)] = ComputeAndReportContrastsFromSpds(['Iteration ' num2str(iter, '%02.0f')] ,theCanonicalPhotoreceptors,T_receptors,...
+        measuredSpd{1}(:, end), measuredSpd{4}(:, end), postreceptoralCombinations, true);
+    [contrastsNegative2(:, iter) postreceptoralContrastsNegative2(:, iter)] = ComputeAndReportContrastsFromSpds(['Iteration ' num2str(iter, '%02.0f')] ,theCanonicalPhotoreceptors,T_receptors,...
+        measuredSpd{1}(:, end), measuredSpd{5}(:, end), postreceptoralCombinations, true);
 end
 
-%% FILTERED
-% Load the filter
-tmp = load(fullfile(materialsPath, 'OneLightCalData/xNDFilters/srf_filter_ND40CassetteA_100516.mat'));
-srf_filter = tmp.srf_filter_ND40CassetteA;
-cassetteID = 'ND40CassetteA';
-
-for d = 1:length(theDirections)
-    % Print out some information
-    fprintf(' * Direction:\t<strong>%s</strong>\n', theDirections{d});
-    fprintf(' * Observer:\t<strong>%s</strong>\n', observerID);
-    fprintf(' * Date:\t<strong>%s</strong>\n', todayDate);
-    
-    % Correct the cache
-    fprintf(' * Starting spectrum-seeking loop...\n');
-    [cacheData olCache spectroRadiometerOBJ] = OLCorrectCacheFileOOC(...
-        fullfile(cacheDir, 'stimuli', ['Cache-' theDirections{d} '.mat']), ...
-        'igdalova@mail.med.upenn.edu', ...
-        'PR-670', spectroRadiometerOBJ, spectroRadiometerOBJWillShutdownAfterMeasurement, ...
-        'FullOnMeas', false, ...
-        'CalStateMeas', false, ...
-        'DarkMeas', false, ...
-        'REFERENCE_OBSERVER_AGE', observerAgeInYrs, ...
-        'ReducedPowerLevels', false, ...
-        'selectedCalType', theCalType, ...
-        'CALCULATE_SPLATTER', false, ...
-        'lambda', 0.8, ...
-        'NIter', 10, ...
-        'powerLevels', [0 1.0000], ...
-        'doCorrection', theDirectionsCorrect(d), ...
-        'postreceptoralCombinations', [1 1 1 0 0 ; 1 -1 0 0 0 ; 0 0 1 0 0 ; 0 0 0 1 0 ; 0 0 0 0 1], ...
-        'outDir', fullfile(materialsPath, 'PIPRMaxPulse', todayDate));
-    fprintf(' * Spectrum seeking finished!\n');
-    
-    % Save the cache
-    fprintf(' * Saving cache ...');
-    params = cacheData.data(observerAgeInYrs).describe.params;
-    params.modulationDirection = theDirections{d};
-    params.cacheFile = ['Cache-' params.modulationDirection '_' cassetteID '_' observerID '_' todayDate '.mat'];
-    OLReceptorIsolateSaveCache(cacheData, olCache, params);
-    fprintf('done!\n');
+% Replace the values in the cache files
+for ii = 1:length(cacheData1.data)
+    if ii == observerAgeInYrs;
+        cacheData1.data(ii).backgroundPrimary = correctedPrimaryValues(:, 1);
+        cacheData1.data(ii).modulationPrimarySignedPositive = correctedPrimaryValues(:, 2);
+        cacheData1.data(ii).modulationPrimarySignedNegative = correctedPrimaryValues(:, 3);
+        cacheData1.data(ii).differencePrimary = cacheData1.data(ii).modulationPrimarySignedPositive - cacheData1.data(ii).backgroundPrimary;
+        cacheData1.data(ii).correction.backgroundPrimaryCorrectedAll = primariesCorrectedAll{1};
+        cacheData1.data(ii).correction.deltaBackgroundPrimaryInferredAll = deltaPrimariesCorrectedAll{1};
+        cacheData1.data(ii).correction.bgSpdAll = measuredSpd{1};
+        cacheData1.data(ii).correction.modulationPrimaryPositiveCorrectedAll = primariesCorrectedAll{2};
+        cacheData1.data(ii).correction.deltaModulationPrimaryPositveInferredAll = deltaPrimariesCorrectedAll{2};
+        cacheData1.data(ii).correction.modPositiveSpdAll = measuredSpd{2};
+        cacheData1.data(ii).correction.modulationPrimaryNegativeCorrectedAll = primariesCorrectedAll{3};
+        cacheData1.data(ii).correction.deltaModulationPrimaryNegativeInferredAll = deltaPrimariesCorrectedAll{3};
+        cacheData1.data(ii).correction.modNegativeSpdAll = measuredSpd{3};
+        cacheData1.data(ii).correction.contrastsPositive = contrastsPositive1;
+        cacheData1.data(ii).correction.postreceptoralContrastsPositive = postreceptoralContrastsPositive1;
+        cacheData1.data(ii).correction.contrastsNegative = contrastsNegative1;
+        cacheData1.data(ii).correction.postreceptoralContrastsNegative = postreceptoralContrastsNegative1;
+    else
+        cacheData1.data(ii).describe = [];
+        cacheData1.data(ii).backgroundPrimary = [];
+        cacheData1.data(ii).backgroundSpd = [];
+        cacheData1.data(ii).differencePrimary = [];
+        cacheData1.data(ii).differenceSpd = [];
+        cacheData1.data(ii).modulationPrimarySignedPositive = [];
+        cacheData1.data(ii).modulationPrimarySignedNegative = [];
+        cacheData1.data(ii).modulationSpdSignedPositive = [];
+        cacheData1.data(ii).modulationSpdSignedNegative = [];
+        cacheData1.data(ii).ambientSpd = [];
+        cacheData1.data(ii).operatingPoint = [];
+        cacheData1.data(ii).computeMethod = [];
+    end
 end
 
-if (~isempty(spectroRadiometerOBJ))
-    spectroRadiometerOBJ.shutDown();
-    spectroRadiometerOBJ = [];
+% Replace the values in the cache files
+for ii = 1:length(cacheData2.data)
+    if ii == observerAgeInYrs;
+        cacheData2.data(ii).backgroundPrimary = correctedPrimaryValues(:, 1);
+        cacheData2.data(ii).modulationPrimarySignedPositive = correctedPrimaryValues(:, 2);
+        cacheData2.data(ii).modulationPrimarySignedNegative = correctedPrimaryValues(:, 3);
+        cacheData2.data(ii).differencePrimary = cacheData2.data(ii).modulationPrimarySignedPositive - cacheData2.data(ii).backgroundPrimary;
+        cacheData2.data(ii).correction.backgroundPrimaryCorrectedAll = primariesCorrectedAll{1};
+        cacheData2.data(ii).correction.deltaBackgroundPrimaryInferredAll = deltaPrimariesCorrectedAll{1};
+        cacheData2.data(ii).correction.bgSpdAll = measuredSpd{1};
+        cacheData2.data(ii).correction.modulationPrimaryPositiveCorrectedAll = primariesCorrectedAll{2};
+        cacheData2.data(ii).correction.deltaModulationPrimaryPositveInferredAll = deltaPrimariesCorrectedAll{2};
+        cacheData2.data(ii).correction.modPositiveSpdAll = measuredSpd{2};
+        cacheData2.data(ii).correction.modulationPrimaryNegativeCorrectedAll = primariesCorrectedAll{3};
+        cacheData2.data(ii).correction.deltaModulationPrimaryNegativeInferredAll = deltaPrimariesCorrectedAll{3};
+        cacheData2.data(ii).correction.modNegativeSpdAll = measuredSpd{3};
+        cacheData2.data(ii).correction.contrasts = contrasts;
+        cacheData2.data(ii).correction.postreceptoralContrasts = postreceptoralContrasts;
+    else
+        cacheData2.data(ii).describe = [];
+        cacheData2.data(ii).backgroundPrimary = [];
+        cacheData2.data(ii).backgroundSpd = [];
+        cacheData2.data(ii).differencePrimary = [];
+        cacheData2.data(ii).differenceSpd = [];
+        cacheData2.data(ii).modulationPrimarySignedPositive = [];
+        cacheData2.data(ii).modulationPrimarySignedNegative = [];
+        cacheData2.data(ii).modulationSpdSignedPositive = [];
+        cacheData2.data(ii).modulationSpdSignedNegative = [];
+        cacheData2.data(ii).ambientSpd = [];
+        cacheData2.data(ii).operatingPoint = [];
+        cacheData2.data(ii).computeMethod = [];
+    end
 end
-toc;
+
+%% Save out the corrected cache files
+OLReceptorIsolateSaveCache(cacheData1, olCache1, params1);
+OLReceptorIsolateSaveCache(cacheData2, olCache2, params2);
