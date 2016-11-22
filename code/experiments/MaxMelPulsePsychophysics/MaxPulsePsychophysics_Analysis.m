663e222ae72ef27eea5247b3b4a67ee6ab8b031f
Manuel Spitschan <spitschan@stanford.edu>
2016-11-22 10:23:04 -0800

    NF: New analysis code

---
+++ code/experiments/MaxMelPulsePsychophysics/MaxPulsePsychophysics_Analysis.m
@@ -0,0 +1,33 @@
+% MaxPulsePsychophysics_Analysis.m
+%
+% Analyses rating data. Hacked up for now.
+%
+% 11/22/16  spitschan  Wrote it.
+
+% Load the file
+load('/Users/spitschan/Downloads/HERO_test3-MaxPulsePsychophysics.mat'); % NOTE: ADJUST PATH
+
+% Get all the stimulus types
+allLabels = {data.stimLabel};
+[uniqueLabels, ~, allLabelsIdx] = unique(allLabels);
+
+% Get all the perceptual dimensions
+allDimensions = {data.perceptualDimension};
+[uniqueDimensions, ~, allDimensionsIdx] = unique(allDimensions);
+
+% Assemble all responses
+allResponses = [data.response];
+
+% Iterate over stimulus types and perceptual rating dimensions
+for ii = 1:length(uniqueLabels)
+   for jj = 1:length(uniqueDimensions)
+      % Find the trials that correspond to this
+      aggregatedData{ii, jj} = allResponses((allLabelsIdx == ii) & (allDimensionsIdx == jj));
+      aggregatedDataMean(ii, jj) = mean(allResponses((allLabelsIdx == ii) & (allDimensionsIdx == jj)));
+   end
+end
+
+% Make a bar plot of the mean ratings
+h = bar(aggregatedDataMean');
+set(gca, 'XTickLabel', uniqueDimensions);
+legend(h, uniqueLabels)
