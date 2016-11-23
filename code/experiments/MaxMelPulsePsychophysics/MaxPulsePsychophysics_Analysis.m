203275644f598533f211f1e276596be27736e2e2
Manuel Spitschan <mspits@sas.upenn.edu>
2016-11-23 09:37:43 -0500

    Proposed edits
    Currently commented out.
    
    Signed-off-by: Manuel Spitschan <mspits@sas.upenn.edu>

--- code/experiments/MaxMelPulsePsychophysics/MaxPulsePsychophysics_Analysis.m
+++ code/experiments/MaxMelPulsePsychophysics/MaxPulsePsychophysics_Analysis.m
@@ -3,8 +3,15 @@
 % Analyses rating data. Hacked up for now.
 %
 % 11/22/16  spitschan  Wrote it.
+% Proposed edits: Add in variable for subject id and date of experiment. 
+
+% Subject information:
+%observerID = HERO_test3
+%expDate = 112216
 
 % Load the file
+% dataPath = getpref('OneLight', 'dataPath');
+%load(fullfile(datapath, observerID, expDate, 'MatFiles', 
 load('/Users/melanopsin/Dropbox (Aguirre-Brainard Lab)/MELA_data/MaxPulsePsychophysics/Hero_test3/112216/MatFiles/HERO_test3-MaxPulsePsychophysics.mat'); 
 % Get all the stimulus types
 allLabels = {data.stimLabel};
