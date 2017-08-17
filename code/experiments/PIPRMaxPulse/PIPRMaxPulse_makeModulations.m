0bc18690cf1b9669e31084f3d96abbfd5babb6c7
Melanopsin Rig <jryan@mail.med.upenn.edu>
2017-08-17 15:30:30 -0400

    new code to facilitate making PIPR modulations at max intensity
    -PIPRMaxPulse_makeModulations serves as a wrapper script. It is to be called in order to make validations
    -it first calls determineMaxPIPR which from the selected calibration and subject’s age determines the strongest blue light stimulus the OneLight can muster
    -it passes this light intensity into the PrepOOC_function to run with prep script with that light intensity as a target
    -then calls the ExptOOC_function script to make the modulations

---
+++ code/experiments/PIPRMaxPulse/PIPRMaxPulse_makeModulations.m
@@ -0,0 +1,20 @@
+
+
+%% get some basic subject information
+commandwindow;
+observerID = GetWithDefault('>> Enter <strong>user name</strong>', 'HERO_test');
+observerAgeInYrs = GetWithDefault('>> Enter <strong>observer age</strong>:', 32);
+takeTemperatureMeasurements = GetWithDefault('Take Temperature Measurements ?', false);
+todayDate = datestr(now, 'mmddyy');
+theCalType = OLGetCalibrationEnumerationType;
+theCalType = theCalType.CalFileName(3:length(theCalType.CalFileName));
+
+%% Determine the strongest blue PIPR stimulus we can deliver based on our most recent PIPR calculation
+[ maxBluePIPRIntensity ] = determineMaxPIPR(observerAgeInYrs, theCalType);
+
+
+%% run the prep script
+PIPRMaxPulse_PrepOOC_function(theCalType, maxBluePIPRIntensity)
+
+%% run the exp script
+PIPRMaxPulse_ExptOOC_function(observerID, observerAgeInYrs, theCalType, takeTemperatureMeasurements)
